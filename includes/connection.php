922b3480b787b6b23ab75fd839f55f9e41270543
TM85dev <64788686+TM85dev@users.noreply.github.com>
2022-03-19 15:14:44 +0100

    create project and connect to database

---
+++ includes/connection.php
@@ -0,0 +1,32 @@
+<?php
+
+    class DB {
+        private $PDO;
+        private $host = 'localhost';
+        private $db = 'sign';
+        private $username = 'root';
+        private $password = '';
+        private $error = null;
+        private $res = '';
+
+        function __construct() {
+            try {
+                $this->PDO = new PDO("mysql:host=$this->host;dbname=$this->db", $this->username, $this->password);
+                $this->PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $this->res = 'connected';
+            } catch(PDOException $e) {
+                $this->error = $e->getMessage();
+            }
+        }
+        public function response() {
+            return ($this->error === null) ? $this->res : false;
+        }
+        public function error() {
+            return $this->error;
+        }
+        function __destruct() {
+            $this->PDO = null;
+        }
+    }
+
+?>
---
+++ includes/user.php
@@ -0,0 +1,24 @@
+<?php
+    include 'connection.php';
+
+    class User extends DB {
+
+        public function login($login, $password) {
+
+        }
+        public function logout() {
+
+        }
+        public function register($username, $email, $password) {
+
+        }
+        public function edit() {
+            
+        }
+        public function delete() {
+
+        }
+
+    }
+
+?>
