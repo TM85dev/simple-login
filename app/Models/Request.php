f24419ab998f490c984462169775aa83adbfcd1b
TM85dev <64788686+TM85dev@users.noreply.github.com>
2022-04-22 19:31:35 +0200

    update methods in Request class

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -8,25 +8,12 @@ class Request {
 
     public function __construct() {
         $this->res = json_decode(file_get_contents('php://input'));
-        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->method = strtolower($_SERVER['REQUEST_METHOD']);
     }
-    public function GET() {
-        return $this->method == 'GET' ? $this->res : null;
+    public function method(string $method = 'get') {
+        return $this->method === strtolower($method) ? $this : false;
     }
-    public function POST() {
-        return $this->method == 'POST' ? $this->res : null;
-    }
-    public function PUT() {
-        return $this->method == 'PUT' ? $this->res : null;
-    }
-    public function PATCH($data_type = null) {
-        if(strtolower($data_type) === 'json') $this->json();
-        return $this->method == 'PATCH' ? $this->res : null;
-    }
-    public function DELETE() {
-        return $this->method == 'DELETE' ? $this->res : null;
-    }
-    private function json() {
-        $this->res = json_encode($this->res);
+    public function format(string $format = 'raw') {
+        return  strtolower($format) === 'json' ? json_encode($this->res) : $this->res;
     }
 }
